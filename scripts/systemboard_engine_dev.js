790bb5be0f1624fd8bcbc1c6783c0ff80437822c
Jeroen van Tilburg <jvantilburg@gmail.com>
2020-05-23 13:23:17 +0200

    Waarschuwing als IE wordt gebruikt.

--- scripts/systemboard_engine_dev.js
+++ scripts/systemboard_engine_dev.js
@@ -38,6 +38,27 @@ fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
 fabric.Textbox.prototype.objectCaching = false;
 fabric.Text.prototype.objectCaching = false;
 
+
+// Set a warning messsage when using Internet Explorer
+function isIE() {
+  // IE 10 and IE 11
+  return /Trident\/|MSIE/.test(window.navigator.userAgent);
+}
+
+let showBrowserAlert = (function () {
+    if (document.querySelector('.unsupported-browser')) {
+        let d = document.getElementsByClassName('unsupported-browser');
+
+        if( isIE() ) {
+            d[0].innerHTML = '<b>Deze browser wordt niet ondersteund!</b></br>Deze webapplicatie werkt niet in Internet Explorer.</br>Gebruik een moderne browser zoals Chrome, Edge, Firefox of Safari.';
+            d[0].style.display = 'block';
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', showBrowserAlert);
+
+
 // Set empty AudioContext, etc
 var audioCtx = null, oscillator = null, gainNode = null;
 
